9f869333860b01838a1ce9272b5778e80f1407d0
dhsimpson <qeadzc9503@naver.com>
2020-10-04 17:33:25 +0900

    app.py에 텍스트를 토큰화 된 텐서로 바꾸는 부분 추가

--- backend/flask_server/app.py
+++ backend/flask_server/app.py
@@ -8,6 +8,16 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+# 동희가 수정한 부분
+# import torch
+# from text_to_tensor import get_tokenizer,text_to_tensor
+# tokenizer = get_tokenizer()
+# 여기는  text to tensor 예시
+# comment = "화련이랑 싸우면 누가 이기려나"
+# res = text_to_tensor(tokenizer, comment)
+# print(res) -> 길이 128인 tensor 데이터 반환(comment 길이 + 나머지는 0 padding)
+# model(res) -> 0,1,2 중 하나
+
 @app.route('/')
 @cross_origin()
 def index():
