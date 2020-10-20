a20efc30e8640286b78b38384514857bcb69ea42
dhsimpson <qeadzc9503@naver.com>
2020-10-20 21:16:28 +0900

    댓글 분류 API 구현

--- backend/flask_server/app.py
+++ backend/flask_server/app.py
@@ -9,17 +9,14 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # 동희가 수정한 부분
-# import torch
-# import json
-# from comment_functions import get_tokenizer,text_to_data, infer
-# tokenizer = get_tokenizer()
+import torch
+import pandas as pd
+import json
+from comment_functions import get_tokenizer,text_to_data, infer
+import comment_model
 
-# 여기는  text to tensor 예시
-# comments = []
-# comments.append("화련이랑 싸우면 누가 이기려나")
-# data = text_to_data(tokenizer, comments)
-# labels = infer(model, data) # json 을 리턴. ex - {"labels" : [0,1,1]}
-
+tokenizer = get_tokenizer()
+model_comment = comment_model.load_model()
 
 @app.route('/')
 @cross_origin()
@@ -40,7 +37,37 @@ def run_model():
 @app.route('/nlp/comment_classify', methods=['GET'])
 @cross_origin()
 def run_comment_classify():
+    df = pd.read_csv('webtoon_comments_labeled.csv',encoding='utf-16')
+    comments = df['comment'].tolist()
+    data = text_to_data(tokenizer, comments[:100])
+    result = infer(model_comment, data, comment_model.device)# label, prediction accuracy 리턴
+    label_0 = []
+    label_1 = []
+    label_2 = []
+    temp_0 = 0
+    temp_1 = 0
+    temp_2 = 0
+    temp_str = ""
+
+    for idx, res in enumerate(result):
+        lbl, acc = res
+        temp_str = comments[idx]
+        if lbl==0 and temp_0<40:
+            label_0.append([ temp_str,lbl,acc[lbl] ])
+            temp_0+=1
+        elif lbl==1 and temp_1<40:
+            label_1.append([ temp_str,lbl,acc[lbl] ])
+            temp_1+=1
+        elif lbl==2 and temp_0<40:
+            label_2.append([ temp_str,lbl,acc[lbl] ])
+            temp_2+=1
+    label_0.sort(key = lambda label_0: label_0[2], reverse=True)
+    label_1.sort(key = lambda label_1: label_1[2], reverse=True)
+    label_2.sort(key = lambda label_2: label_2[2], reverse=True)
     
+    return jsonify({"label_0":label_0,
+                        "label_1":label_1,
+                        "label_2":label_2})
     #return으로 json 결과를 주떼엽><
 
 
