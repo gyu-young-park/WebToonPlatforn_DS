c84ff36f4862991a70cfd4b0144aa95bae799c4a
jartfo <jartfo@gmail.com>
2020-10-22 18:25:04 +0900

    encoder, decoder modified, add&change stegano model for image form, get out .steg from folder and delete folder

--- backend/flask_server/stegano_encoder.py
+++ backend/flask_server/stegano_encoder.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3.6
 import os
 
-def _stegano_encode(cover_img, output_stegano_img, msg, steg = os.path.join("/stegano_pretrained/", "dense")):
-	from steganogan import SteganoGAN
-	from steganogan.encoders import DenseEncoder
+def _stegano_encode(cover_img, output_stegano_img, msg, steg = os.path.join("./", "dense")):
+	from stegano_model_ import SteganoGAN
 
 	steganogan = SteganoGAN.load(steg, cuda=False)
 	steganogan.encode(cover_img, output_stegano_img, msg)
